4bcddfa51b93c5d33efafe6bb4f54463db5ab84e
Shahjahan Arfin <shahjahanarfin321@gmail.com>
2023-03-26 11:32:49 +0530

    Created Users entity for operating on users

---
+++ Backend/medicare/src/main/java/com/ey/medicare/entity/UsersEntity/Users.java
@@ -0,0 +1,18 @@
+package com.ey.medicare.entity.UsersEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
