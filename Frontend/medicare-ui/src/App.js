47b4592cbfe76fa55ae6abef06ed56c9ea2d8f94
Shahjahan Arfin <shahjahanarfin321@gmail.com>
2023-03-29 16:17:16 +0530

    created the login form using bootstrapt with responsive navbar

--- Frontend/medicare-ui/src/App.js
+++ Frontend/medicare-ui/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import NavbarCollapse from './components/NavbarCollapse';
+import Navbar from './components/NavbarCollapse';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarCollapse />
+      <div className="App">
+        <header className="App-header">
+          <Login />
+        </header>
+      </div>
     </div>
   );
 }
