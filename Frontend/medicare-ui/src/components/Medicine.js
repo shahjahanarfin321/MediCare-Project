eef85a6ef37956ebe8302e476b47eee22311c966
Shahjahan Arfin <shahjahanarfin321@gmail.com>
2023-04-03 19:51:56 +0530

    added the functionality to add product to cart and disable the add to cart button but the issue is the button again activate when I visit the page medicine again

--- Frontend/medicare-ui/src/components/Medicine.js
+++ Frontend/medicare-ui/src/components/Medicine.js
@@ -2,7 +2,11 @@ import { useState, useEffect } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import axios from 'axios';
 
-function MedicineCard({ medicineName, medicinePrice, medicineStock }) {
+function MedicineCard({ medicineName, medicinePrice, medicineStock, medicineId, onAddToCart, disabled }) {
+  const handleAddToCart = () => {
+    onAddToCart(medicineId);
+  };
+
   return (
     <Col md={4}>
       <Card className="mb-4">
@@ -14,7 +18,9 @@ function MedicineCard({ medicineName, medicinePrice, medicineStock }) {
             <br />
             Stock: {medicineStock}
           </Card.Text>
-          <Button variant="primary">Add to Cart</Button>
+          <Button variant="primary" onClick={handleAddToCart} disabled={disabled}>
+            {disabled ? 'Added to Cart' : 'Add to Cart'}
+          </Button>
         </Card.Body>
       </Card>
     </Col>
@@ -23,6 +29,7 @@ function MedicineCard({ medicineName, medicinePrice, medicineStock }) {
 
 function Medicine() {
   const [medicines, setMedicines] = useState([]);
+  const [addedToCart, setAddedToCart] = useState([]);
 
   useEffect(() => {
     fetchMedicines();
@@ -45,6 +52,19 @@ function Medicine() {
     setMedicines(sortedMedicines);
   };
 
+  const handleAddToCart = (medicineId) => {
+    axios.post(`http://localhost:9090/cart/add-medicine-to-cart?medicineId=${medicineId}&userId=${localStorage.getItem("userId")}&quantity=1`)
+      .then(response => {
+        console.log(response.data);
+        setAddedToCart([...addedToCart, medicineId]);
+        alert('Item added to cart successfully!');
+      })
+      .catch(error => {
+        console.log(error);
+        alert('Error occurred while adding item to cart!');
+      });
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
       fetchMedicines();
@@ -63,18 +83,58 @@ function Medicine() {
       </div>
       <Row>
         {medicines.map((medicine) => (
-          <MedicineCard key={medicine.medicineId} {...medicine} />
+          <MedicineCard
+            key={medicine.medicineId}
+            {...medicine}
+            onAddToCart={handleAddToCart}
+            disabled={addedToCart.includes(medicine.medicineId)}
+          />
         ))}
       </Row>
     </Container>
   );
 }
 
+export default Medicine;
+
+
+// import { useState, useEffect } from 'react';
+// import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+// import axios from 'axios';
+
+// function MedicineCard({ medicineName, medicinePrice, medicineStock, medicineId, onAddToCart }) {
+//   const handleAddToCart = () => {
+//     onAddToCart(medicineId);
+//   };
+
+//   return (
+//     <Col md={4}>
+//       <Card className="mb-4">
+//         <Card.Img variant="top" src="https://via.placeholder.com/400x400" />
+//         <Card.Body>
+//           <Card.Title>{medicineName}</Card.Title>
+//           <Card.Text>
+//             ${medicinePrice}
+//             <br />
+//             Stock: {medicineStock}
+//           </Card.Text>
+//           <Button variant="primary" onClick={handleAddToCart}>
+//             Add to Cart
+//           </Button>
+//         </Card.Body>
+//       </Card>
+//     </Col>
+//   );
+// }
 
 // function Medicine() {
 //   const [medicines, setMedicines] = useState([]);
 
 //   useEffect(() => {
+//     fetchMedicines();
+//   }, []);
+
+//   const fetchMedicines = () => {
 //     axios.get('http://localhost:9090/medicine/all-medicines')
 //       .then(response => {
 //         setMedicines(response.data);
@@ -82,7 +142,7 @@ function Medicine() {
 //       .catch(error => {
 //         console.log(error);
 //       });
-//   }, []);
+//   }
 
 //   const handleSortByName = () => {
 //     const sortedMedicines = [...medicines].sort((a, b) =>
@@ -91,41 +151,63 @@ function Medicine() {
 //     setMedicines(sortedMedicines);
 //   };
 
+//   const handleAddToCart = (medicineId) => {
+//     axios.post(`http://localhost:9090/cart/add-medicine-to-cart?medicineId=${medicineId}&userId=${localStorage.getItem("userId")}&quantity=1`)
+//       .then(response => {
+//         console.log(response.data);
+//         alert('Item added to cart successfully!');
+//       })
+//       .catch(error => {
+//         console.log(error);
+//         alert('Error occurred while adding item to cart!');
+//       });
+//   };
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       fetchMedicines();
+//     }, 2000); // fetch medicines every 5 seconds
+
+//     return () => clearInterval(interval);
+//   }, []);
+
 //   return (
 //     <Container className="my-5">
 //       <div className="d-flex justify-content-between align-items-center">
 //         <h1 className="mb-0">Medicines</h1>
 //         <Button variant="primary" onClick={handleSortByName}>
-//           {/* <FaSort /> */}
 //           Sort
 //         </Button>
 //       </div>
 //       <Row>
 //         {medicines.map((medicine) => (
-//           <MedicineCard key={medicine.medicineId} {...medicine} />
+//           <MedicineCard key={medicine.medicineId} {...medicine} onAddToCart={handleAddToCart} />
 //         ))}
 //       </Row>
 //     </Container>
 //   );
 // }
 
-export default Medicine;
+// export default Medicine;
 
 
 
-
-// import { useState } from 'react';
+// import { useState, useEffect } from 'react';
 // import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-// // import { FaSort } from 'react-icons/fa';
+// import axios from 'axios';
 
-// function MedicineCard({ name, price, imageUrl }) {
+// function MedicineCard({ medicineName, medicinePrice, medicineStock }) {
 //   return (
 //     <Col md={4}>
 //       <Card className="mb-4">
-//         <Card.Img variant="top" src={imageUrl} />
+//         <Card.Img variant="top" src="https://via.placeholder.com/400x400" />
 //         <Card.Body>
-//           <Card.Title>{name}</Card.Title>
-//           <Card.Text>${price}</Card.Text>
+//           <Card.Title>{medicineName}</Card.Title>
+//           <Card.Text>
+//             ${medicinePrice}
+//             <br />
+//             Stock: {medicineStock}
+//           </Card.Text>
 //           <Button variant="primary">Add to Cart</Button>
 //         </Card.Body>
 //       </Card>
@@ -134,44 +216,48 @@ export default Medicine;
 // }
 
 // function Medicine() {
-//   const [medicines, setMedicines] = useState([
-//     {
-//       name: 'Medicine 1',
-//       price: 90.99,
-//       imageUrl: 'https://via.placeholder.com/400x400',
-//     },
-//     {
-//       name: 'Medicine 2',
-//       price: 15.99,
-//       imageUrl: 'https://via.placeholder.com/400x400',
-//     },
-//     {
-//       name: 'Medicine 3',
-//       price: 20.99,
-//       imageUrl: 'https://via.placeholder.com/400x400',
-//     },
-//     // Add more medicines here...
-//   ]);
+//   const [medicines, setMedicines] = useState([]);
+
+//   useEffect(() => {
+//     fetchMedicines();
+//   }, []);
+
+//   const fetchMedicines = () => {
+//     axios.get('http://localhost:9090/medicine/all-medicines')
+//       .then(response => {
+//         setMedicines(response.data);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
 
 //   const handleSortByName = () => {
 //     const sortedMedicines = [...medicines].sort((a, b) =>
-//       a.name.localeCompare(b.name)
+//       a.medicineName.localeCompare(b.medicineName)
 //     );
 //     setMedicines(sortedMedicines);
 //   };
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       fetchMedicines();
+//     }, 2000); // fetch medicines every 5 seconds
+
+//     return () => clearInterval(interval);
+//   }, []);
 
 //   return (
 //     <Container className="my-5">
 //       <div className="d-flex justify-content-between align-items-center">
 //         <h1 className="mb-0">Medicines</h1>
 //         <Button variant="primary" onClick={handleSortByName}>
-//           {/* <FaSort /> */}
 //           Sort
 //         </Button>
 //       </div>
 //       <Row>
-//         {medicines.map((medicine, index) => (
-//           <MedicineCard key={index} {...medicine} />
+//         {medicines.map((medicine) => (
+//           <MedicineCard key={medicine.medicineId} {...medicine} />
 //         ))}
 //       </Row>
 //     </Container>
